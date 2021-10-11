564a3bb1c3bbe1690906b756c75d2e6964340b57
dev-jun <jun094@naver.com>
2021-10-11 23:29:57 +0900

    webpack.config.js 파일 설정

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,15 @@ const newPath = path.resolve(__dirname, "abc");
 console.log(newPath); // '/Users/goorm/Documents/dev/webpack-tutorial/abc'
 */
 
+const path = require("path");
+
 module.exports = {
   entry: "/src/index.js",
-  output: "/dist",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js", //dist 폴더내에 bundle된 파일의 명
+  },
+  // 이 웹이 node 환경에서 이루어진다는 것을 명시해줌.
+  // 이걸 안해주면 cli 에서 'npx webpack --target=node' 이렇게 일일히 명시 해주어야함.
+  target: "node",
 };
